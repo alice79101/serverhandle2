b2e08127b56e7f911c0469636520e84b3890eb0e
Mia <alice79101@gmail.com>
2023-04-11 15:26:28 +0800

    feat: login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,7 +14,12 @@ class AuthController extends Controller
             'email' => 'required|email|min:3|max:255',
             'password' => 'required|alpha_num:|min:6|max:255',
         ]);
+        $token = Auth::guard('api')->attempt($credential);
+        abort_if( !$token, Response::HTTP_BAD_REQUEST, "帳號或密碼錯誤");
 
-        return "Hi loginnn";
+        return \response([
+            'message' => 'login successful',
+            'data' => $token,
+        ]);
     }
 }
